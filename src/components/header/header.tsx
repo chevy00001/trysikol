59746d2d7dbc913a0cb1bdf58866c1a1b9e2023f
chevy <chevy.manguiob@saratelsolutions.ph>
2024-05-09 11:52:35 +0800

    Made header a link and redirect to booking page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 import styles from "./mystyles.module.css";
+import Link from "next/link";
 const Header = () => {
   return (
     <>
-      <div className={styles.container}>
-        <div>
-          {" "}
-          <Image
-            src="/logo.svg"
-            width={71}
-            height={57}
-            className=""
-            alt="Trysikol Logo"
-          />
-        </div>
-        <div className={styles.headerContainer}>
-          <div className={styles.header}>
-            Trysikol <span className={styles.copyright}>&#169;</span>
+      <Link href="/booking">
+        <div className={styles.container}>
+          <div>
+            {" "}
+            <Image
+              src="/logo.svg"
+              width={71}
+              height={57}
+              className=""
+              alt="Trysikol Logo"
+            />
           </div>
-          <div className={styles.subheader}>Book Your Safest Ride Ever!</div>
+          <div className={styles.headerContainer}>
+            <div className={styles.header}>
+              Trysikol <span className={styles.copyright}>&#169;</span>
+            </div>
+            <div className={styles.subheader}>Book Your Safest Ride Ever!</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
