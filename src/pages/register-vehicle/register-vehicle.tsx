f4bfe91fa9565e9e4a36f4a4a22a2757a8cde9c7
chevy <chevy.manguiob@saratelsolutions.ph>
2024-05-09 11:55:46 +0800

    1. Fixed register vehicle position

--- src/pages/register-vehicle/register-vehicle.tsx
+++ src/pages/register-vehicle/register-vehicle.tsx
@@ -7,80 +7,82 @@ import Button from "@/components/button/button";
 const RegisterVehicle = () => {
   return (
     <div className="container">
-      <div>
-        <Header />
-      </div>
-      <div className={styles.documentHeader}>
-        <span className={styles.documentLabel}>Add additional documents</span>
-      </div>
-      <Input
-        type={"file"}
-        label={"Upload your photo"}
-        id={"userPhoto"}
-        name={"userPhoto"}
-      />
-      <Input
-        label={"License Number"}
-        type={"text"}
-        id={"licenseNumber"}
-        name={"licenseNumber"}
-      />
-      <Input
-        type={"file"}
-        label={"Upload your license"}
-        id={"license"}
-        name={"license"}
-      />
-      <Input
-        label={"Plate Number"}
-        type={"text"}
-        id={"plateNumber"}
-        name={"plateNumber"}
-      />
-      <Input
-        label={"Body Number"}
-        type={"text"}
-        id={"bodyNumber"}
-        name={"bodyNumber"}
-      />
-      <Select label={"Type of vehicle"} />
-      <Input
-        type={"file"}
-        label={"Upload tricycle photo"}
-        id={"tricyclePhoto"}
-        name={"tricyclePhoto"}
-      />
-      <Input
-        label={"Toda Name (Optional)"}
-        type={"text"}
-        id={"bodyNumber"}
-        name={"bodyNumber"}
-      />
-      <div className={styles.documentHeader}>
-        <span className={styles.documentLabel}>Address</span>
-      </div>
-      <Input
-        label={"Province"}
-        type={"text"}
-        id={"province"}
-        name={"province"}
-      />
-      <Input label={"City"} type={"text"} id={"city"} name={"city"} />
-      <Input
-        label={"Barangay"}
-        type={"text"}
-        id={"barangay"}
-        name={"barangay"}
-      />
-      <Input
-        label={"Street Address, House Number"}
-        type={"text"}
-        id={"streetAddress"}
-        name={"streetAddress"}
-      />
+      <div className="center">
+        <div>
+          <Header />
+        </div>
+        <div className={styles.documentHeader}>
+          <span className={styles.documentLabel}>Add additional documents</span>
+        </div>
+        <Input
+          type={"file"}
+          label={"Upload your photo"}
+          id={"userPhoto"}
+          name={"userPhoto"}
+        />
+        <Input
+          label={"License Number"}
+          type={"text"}
+          id={"licenseNumber"}
+          name={"licenseNumber"}
+        />
+        <Input
+          type={"file"}
+          label={"Upload your license"}
+          id={"license"}
+          name={"license"}
+        />
+        <Input
+          label={"Plate Number"}
+          type={"text"}
+          id={"plateNumber"}
+          name={"plateNumber"}
+        />
+        <Input
+          label={"Body Number"}
+          type={"text"}
+          id={"bodyNumber"}
+          name={"bodyNumber"}
+        />
+        <Select label={"Type of vehicle"} />
+        <Input
+          type={"file"}
+          label={"Upload tricycle photo"}
+          id={"tricyclePhoto"}
+          name={"tricyclePhoto"}
+        />
+        <Input
+          label={"Toda Name (Optional)"}
+          type={"text"}
+          id={"bodyNumber"}
+          name={"bodyNumber"}
+        />
+        <div className={styles.documentHeader}>
+          <span className={styles.documentLabel}>Address</span>
+        </div>
+        <Input
+          label={"Province"}
+          type={"text"}
+          id={"province"}
+          name={"province"}
+        />
+        <Input label={"City"} type={"text"} id={"city"} name={"city"} />
+        <Input
+          label={"Barangay"}
+          type={"text"}
+          id={"barangay"}
+          name={"barangay"}
+        />
+        <Input
+          label={"Street Address, House Number"}
+          type={"text"}
+          id={"streetAddress"}
+          name={"streetAddress"}
+        />
 
-      <div className="pt-16">
-        <Button label={"Register"} className={"btnPrimary"} />
+        <div className="pt-16">
+          <Button label={"Register"} className={"btnPrimary"} />
+        </div>
       </div>
     </div>
   );
